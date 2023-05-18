74d5b23d39fa41761fa798df12390e4cccf851f9
s1290087 <s1290087.u-aizu.ac.jp>
2023-05-18 17:29:27 +0900

    WIP

--- Ex11/ServerEnvClient.java
+++ Ex11/ServerEnvClient.java
@@ -3,15 +3,15 @@ import java.rmi.RemoteException;
 
 public class ServerEnvClient {
     public static void main(String args[]) {
-        Info info = null;        
+	String[] infos = new String[2];
         ServerEnv obj = null;
         try {
 	    obj = (ServerEnv)Naming.lookup("//localhost:4321/ServerEnv");
-            info = obj.getInfo(); 
+            infos = obj.getInfo(); 
         } catch (Exception e) { 
             System.out.println("ServerEnvClient exception: " + e.getMessage()); 
             e.printStackTrace(); 
         } 
-        System.out.println(info.toString());
+        System.out.println("Server Env: OS of " + infos[0] + " is " + infos[1] + "." );
     }
 }
